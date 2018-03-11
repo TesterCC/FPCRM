f65c25d99f070475f91124a8e8fde8e050102843
MFC <liyanxi07@gmail.com>
2018-03-12 02:01:06 +0800

    add: start design king_admin.

--- apps/king_admin/urls.py
+++ apps/king_admin/urls.py
@@ -11,14 +11,14 @@ Class-based views
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls', namespace="xxx"))
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from king_admin import views
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^crm/', include("crm.urls")),
-    url(r'^student/', include("student.urls")),
-    url(r'^king_admin/', include("king_admin.urls")),
+    url(r'^$', views.index, name='table_index'),
+
 ]
